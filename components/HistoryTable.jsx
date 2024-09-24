ccc4ab62f5ea864a08fce4732adcf48ecf591435
Marina <marina.lap08@gmail.com>
2024-09-24 13:28:47 +0200

    implement sorting by room and by date in HistoryTable component

--- components/HistoryTable.jsx
+++ components/HistoryTable.jsx
@@ -1,22 +1,63 @@
-export default function HistoryTable({ history }) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Room</th>
-          <th>Date</th>
-          <th>Temperature</th>
-          <th>Humidity</th>
-          <th>Changed by</th>
-        </tr>
-      </thead>
+"use client";
 
-      <tbody>
-        {history.map((record, index) => (
-          <HistoryRow key={index} record={record} />
-        ))}
-      </tbody>
-    </table>
+import { useState } from "react";
+
+const convertObjectToArray = (obj) => {
+  return Object.values(obj);
+};
+
+export default function HistoryTable({ history }) {
+  const [sortOption, setSortOption] = useState("");
+
+  const historyArray = convertObjectToArray(history);
+
+  const sortedHistory = [...historyArray].sort((a, b) => {
+    if (sortOption === "date") {
+      return b.changeAt - a.changeAt;
+    } else if (sortOption === "room") {
+      return a.roomId.name.localeCompare(b.roomId.name);
+    }
+
+    return 0;
+  });
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="sortOption">Sort by: </label>{" "}
+        <select
+          id="sortOption"
+          value={sortOption}
+          onChange={(e) => setSortOption(e.target.value)}
+        >
+          <option value="">No sorting</option>
+          <option value="date">Sort by date</option>
+          <option value="room">Sort by room</option>
+        </select>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Room</th>
+            <th>Date</th>
+            <th>Temperature</th>
+            <th>Humidity</th>
+            <th>Changed by</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedHistory.length > 0 ? (
+            sortedHistory.map((record, index) => (
+              <HistoryRow key={index} record={record} />
+            ))
+          ) : (
+            <tr>
+              <td colSpan="5">No records found</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
