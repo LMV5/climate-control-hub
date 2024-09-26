7bfb188815c6c40d8647e2cf6bdd1ac0554bb013
Marina <marina.lap08@gmail.com>
2024-09-26 10:59:42 +0200

    retrieve room's name instead of room's id in room's history

--- utils/getRoomHistory.js
+++ utils/getRoomHistory.js
@@ -2,8 +2,17 @@ import History from "@/models/History";
 
 export const getRoomHistory = async (roomId) => {
   try {
-    const roomHistory = await History.find({ roomId: roomId }).lean();
-    return roomHistory;
+    const roomHistory = await History.find({ roomId })
+      .populate("roomId", "name")
+      .lean();
+
+    const cleanHistory = roomHistory.map((record) => ({
+      ...record,
+      _id: record._id.toString(),
+      roomId: record.roomId.name,
+    }));
+
+    return cleanHistory;
   } catch (error) {
     console.error("Error fetching room history:", error);
     return [];
